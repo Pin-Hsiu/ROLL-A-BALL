eb2c91180e0413a576b30a839e23a5e9b213b68c
Pin-Hsiu <j1152776@gmail.com>
2015-11-30 14:54:35 +0800

    +計時器
    無法到0停住

---
+++ Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    public static int time;
+    int timer_i = 10;
+    Text text;
+
+    void Start()
+    {
+        InvokeRepeating("timer", 1f, 1f);
+        text = GetComponent<Text>();
+        time = 0;
+    }
+    void timer()
+    {
+        timer_i -= 1;
+        Debug.Log(timer_i);
+    }
+
+    void Update()
+    {
+        text.text = "TIME: " + timer_i;
+    }
+}
